c580c6d95b9348fbf24aef6b4fcbeba484859b05
Kazik <kazgancza@gmail.com>
2023-07-06 19:35:32 +0200

    is prime func

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ print("Hello GitHub!")
 for i in range(0,100):
     print(i)
 
+def is_prime(number):
+    for i in range(2,number):
+        if number % i == 0 and i != number:
+            return False
+    return True
+
+    
+
+print(is_prime(7))
+print(is_prime(6))
